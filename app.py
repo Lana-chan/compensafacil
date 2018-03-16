aa5ce5fcf9ce7711344b41ae82b2daf5f44c2c74
Erin Pinheiro <Lana-chan@users.noreply.github.com>
2018-03-16 16:04:33 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, url_for
+import matcher
+import locale
+app = Flask(__name__)
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+empreendedores = matcher.Empreendedor.load_from_csv('empreendedores.csv')
+proprietarios = matcher.Proprietario.load_from_csv('proprietarios.csv')
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/empreendedores')
+def pag_empre():
+  return render_template('empreendedores.html', empr=empreendedores)
+
+@app.route('/empreendedor/<tcra>')
+def matches_empre(tcra):
+  empr = next((x for x in empreendedores if x.tcra == tcra), None)
+  return render_template('matches.html', empr=empr, matches=empr.find_matches(proprietarios))
+
+@app.route('/proprietarios')
+def pag_propri():
+  return render_template('proprietarios.html', propri=proprietarios)
+
+  
+@app.context_processor
+def utility_processor():
+  def bioma(bio):
+    return u'{}'.format(matcher.bioma_textual(bio))
+
+  def prioridade(pri):
+    return u'{}'.format(matcher.prioridade_textual(pri))
+    
+  def preco(num):
+    return locale.format(u'%.2f', num, True)
+    
+  def number(num):
+    return locale.format(u'%.f' if num % 1 else u'%.0f', num, True)
+
+  return dict(bioma=bioma, prioridade=prioridade, int=int, preco=preco, number=number)
+
+if __name__ == '__main__':
+  app.jinja_env.auto_reload = True
+  app.config['TEMPLATES_AUTO_RELOAD'] = True
+  app.run(debug=True, host='0.0.0.0')
---
+++ matcher.py
@@ -0,0 +1,119 @@
+from unidecode import unidecode
+import csv
+
+class Empreendedor:
+  def __init__(self, tcra, area, bioma, prioridade):
+    self.tcra = tcra
+    self.area = area
+    self.bioma = bioma
+    self.prioridade = prioridade
+    
+  def load_from_csv(filename):
+    entradas = []
+    with open(filename, 'r') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+        item = Empreendedor(row["nº TCRA/TAC"],
+                            float(row["Área (m²)"]),
+                            bioma_numerico(row["Bioma"]),
+                            prioridade_numerica(row["Prioridade"]))
+        item.municipio = row["Município"]
+        entradas.append(item)
+    return entradas
+    
+  """
+    determina lista de proprietários que compensam a demanda do empreendedor
+    proprietário precisa ter:
+      área maior ou igual à demanda
+      prioridade maior ou igual à demanda
+      bioma igual à demanda
+  """
+  def find_matches(self, proprietarios):
+    results = list(filter(lambda x: x.area >= self.area and
+                                    x.prioridade >= self.prioridade and
+                                    x.bioma == self.bioma,
+                                    proprietarios))
+    return results
+    
+  def custo(self, propriedade):
+    if hasattr(propriedade, "preco"):
+      return self.area * propriedade.preco
+    return None
+    
+  def __str__(self):
+    e = "TCRA/TAC: {} - {} m2 de {} com prioridade {}".format(self.tcra,
+                                                              self.area,
+                                                              bioma_textual(self.bioma),
+                                                              prioridade_textual(self.prioridade).lower())
+    if hasattr(self, 'municipio'):
+      e = e + ", {}".format(self.municipio)
+    return e
+    
+class Proprietario:
+  def __init__(self, matricula, area, bioma, prioridade):
+    self.matricula = matricula
+    self.area = area
+    self.bioma = bioma
+    self.prioridade = prioridade
+    
+  def load_from_csv(filename):
+    entradas = []
+    with open(filename, 'r') as csvfile:
+      reader = csv.DictReader(csvfile)
+      for row in reader:
+        item = Proprietario(row["Nº Matrícula"],
+                            float(row["Área disponível (m²)"]),
+                            bioma_numerico(row["Bioma"]),
+                            prioridade_numerica(row["Prioridade"]))
+        item.municipio = row["Município"]
+        item.proprietario = row["Proprietário"]
+        item.preco = float(row["R$/m²"])
+        entradas.append(item)
+    return entradas
+    
+  def __str__(self):
+    e = "Matrícula: {} - {} m2 de {} com prioridade {}".format(self.matricula,
+                                                               self.area,
+                                                               bioma_textual(self.bioma),
+                                                               self.prioridade)
+    if hasattr(self, 'municipio'):
+      e = e + ", {}".format(self.municipio)
+    return e
+
+# converte prioridade em texto para valor numérico    
+def prioridade_numerica(string):
+  # trata string, tirando acentos, transformando em minuscula e removendo espacos
+  new_str = unidecode(string).lower().strip()
+  
+  pri = {"baixa":1,
+          "media":2,
+          "alta":3,
+          "muito alta":4}
+  
+  return pri[new_str]
+  
+# converte prioridade em número para texto
+def prioridade_textual(prioridade):
+  pri = ["Baixa",
+         "Média",
+         "Alta",
+         "Muito Alta"]
+         
+  return pri[prioridade-1]
+
+# converte bioma em texto para valor numérico  
+def bioma_numerico(string):
+  # trata string, tirando acentos, transformando em minuscula e removendo espacos
+  new_str = unidecode(string).lower().strip()
+  
+  bio = {"mata atlantica":1,
+          "cerrado":2}
+  
+  return bio[new_str]
+  
+# converte bioma em código para nome
+def bioma_textual(bioma):
+  bio = ["Mata Atlântica",
+          "Cerrado"]
+        
+  return bio[bioma-1]
